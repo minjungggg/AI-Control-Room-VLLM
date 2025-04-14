33e1ddf53349e7f20c765355e6dc29e51736b70c
yncity <kty884@gmail.com>
2025-04-14 18:20:21 +0900

    gpt에게 사진 정보 전달 & 답변 듣기 & entry_point에 추가 & build 완료

--- ros_gz_ai_control/setup.py
+++ ros_gz_ai_control/setup.py
@@ -30,6 +30,7 @@ setup(
             'image_sub = ros_gz_ai_control.get_image.image_sub:main',
             'image_saver = ros_gz_ai_control.get_image.image_saver:main',
             'thrust_control = ros_gz_ai_control.thrust_control.thrust_control:main',
+            'gpt_image_describer = ros_gz_ai_control.thrust_control.gpt_talker:main',
         ],
     },
 )
